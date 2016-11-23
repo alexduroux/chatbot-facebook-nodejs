8f13345a0e98f46e7e1ccc022f433aee30977bdc
Alex Duroux <aduroux@Alexs-MacBook-Pro-2.local>
2016-11-23 14:42:50 -0500

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAFncoUgqwMBAPj9lpmruY2MzfqLfJCtIKXsan4UveVKnzVKLjKKYJxB8672VixYWZBKey6LvlMKrD3dVsebGZCGZAZAC5JkrdC2cUpAbT4xgpmeUdfh4t5P5ZAh1AdZBp2xkVL1X0fh1xQ4OyvDCGeQdtNRQ665zR5fVMybcZCEAZDZD',
+    FB_VERIFY_TOKEN: 'my_verify_fb__ck_token',
+    API_AI_CLIENT_ACCESS_TOKEN: 'da48003b7a5b4e25913d4a82c60fa7ef',
+    FB_APP_SECRET: 'f2d7151e08fc5801aa249b781b00be20',
+    SERVER_URL: "https://ckbot.herokuapp.com",
 };
